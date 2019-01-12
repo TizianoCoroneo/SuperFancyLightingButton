a431b497a3c528a1b0b3dbe44b5b7e9349c412bb
Tiziano Coroneo <tizianocoroneo@me.com>
2019-01-12 19:24:10 +0100

    Made some base reflective surfaces

--- SuperFancyLightingButton/ViewController.swift
+++ SuperFancyLightingButton/ViewController.swift
@@ -7,14 +7,53 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var sceneView: ARSCNView!
+
+    private lazy var buttonNode =
+        SCNScene(named: "Button.scn")!.rootNode
+            .childNode(
+                withName: "Container",
+                recursively: false)!
+
+    private var configuration: ARWorldTrackingConfiguration!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        startARKit()
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
+        sceneView.automaticallyUpdatesLighting = true
+        sceneView.session.run(configuration)
+
+        guard let pov = sceneView
+            .defaultCameraController
+            .pointOfView
+            else { return }
+
+        let newCopy = buttonNode.flattenedClone()
+
+        pov.addChildNode(newCopy)
+        newCopy.simdPosition = simd_float3(0, 0, -3)
+        newCopy.simdScale = simd_float3(1, 1, 1)
+
+        sceneView.backgroundColor = UIColor.clear
+        sceneView.scene.background.contents = UIColor.clear
+    }
+
+    func startARKit() {
+        configuration = ARWorldTrackingConfiguration()
+        // Enable automatic environment texturing
+        configuration.environmentTexturing = .automatic
+        configuration.isAutoFocusEnabled = false
+    }
 }
 
